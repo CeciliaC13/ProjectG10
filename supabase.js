9685d3c3614def0a08699343105545eccc27659a
Hydex106 <adamnafhan106@gmail.com>
2026-05-06 18:53:31 +0800

    Verification done

--- supabase.js
+++ supabase.js
@@ -2,4 +2,9 @@ const SUPABASE_URL = 'https://lpqzdcdinynhsbmekfnl.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwcXpkY2RpbnluaHNibWVrZm5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MTg1MzgsImV4cCI6MjA5MzA5NDUzOH0.OPeBGPkprKZ1fJDHJq8VcakGOuW-E2jHGFG8_Di8oCQ'
 
 const { createClient } = supabase
-const db = createClient(SUPABASE_URL, SUPABASE_KEY)
+const db = createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: {
+    detectSessionInUrl: true,  // ← add this
+    persistSession: true        // ← add this
+  }
+})
